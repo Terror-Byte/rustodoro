901f72d04ccdf6dbd037f3d63389b2489bef59e8
Liam Swain <liamswain4@gmail.com>
2024-02-03 16:58:53 +0000

    Implemented simple argument parsing to take in an integer to specify in seconds how long the timer runs for

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use std::time::Instant;
+use std::env;
 
 fn main() {
-    println!("Timer time!");
-    run_timer(60);
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        println!("No arguments supplied.");
+        return;
+    } else if args.len() > 2 {
+        println!("Too many arguments supplied.");
+        return;
+    }
+
+    // Attempt to parse the first argument as an unsigned 64-bit int.
+    // If success, run timer.
+    // If failure, return error.
+    match &args[1].parse::<u64>() {
+        Ok(n) => run_timer(n),
+        Err(e) => println!("Failed to parse argument as integer! Error: {}", e),
+    }
 }
 
-fn run_timer(time_in_secs: u64) {
+fn run_timer(time_in_secs: &u64) {
     let start = Instant::now();
-    print_time_remaining(time_in_secs);
+    print_time_remaining(*time_in_secs);
 
     let mut old_printed_value: u64 = 0;
     loop {
@@ -19,46 +35,10 @@ fn run_timer(time_in_secs: u64) {
             old_printed_value = elapsed_seconds;
         }
 
-        if elapsed_seconds >= time_in_secs {
-            break; // Replace this with a return? Can make the function
-        }
-    }
-    println!("Timer elapsed!");
-}
-
-fn print_time_remaining(time_remaining: u64) {
-    print!("{esc}c", esc = 27 as char); // Hacky way of clearing the console! https://stackoverflow.com/questions/34837011/how-to-clear-the-terminal-screen-in-rust-after-a-new-line-is-printed
-    println!("{} seconds to go.", time_remaining);
-}
-
-/*
-use std::time::Instant;
-
-// Useful examples:
-// let seconds = Duration::from_secs(10); (Duration lives in std::time::Duration).
-// while start.elapsed().as_secs() < 5 { } <- do stuff whilst the timer hasn't elapsed
-// Had use core::time and use std::time::Duration initially but didn't end up using them.
-
-const TIME_IN_SECS: u64 = 60;
-fn main() {
-    let start = Instant::now();
-    print_time_remaining(TIME_IN_SECS);
-
-    let mut old_printed_value: u64 = 0;
-    loop {
-        let elapsed_seconds = start.elapsed().as_secs();
-
-        if elapsed_seconds > old_printed_value {
-            let time_remaining = TIME_IN_SECS - elapsed_seconds;
-            print_time_remaining(time_remaining);
-            old_printed_value = elapsed_seconds;
-        }
-
-        if elapsed_seconds >= TIME_IN_SECS {
+        if elapsed_seconds >= *time_in_secs {
             break;
         }
     }
-
     println!("Timer elapsed!");
 }
 
@@ -66,4 +46,10 @@ fn print_time_remaining(time_remaining: u64) {
     print!("{esc}c", esc = 27 as char); // Hacky way of clearing the console! https://stackoverflow.com/questions/34837011/how-to-clear-the-terminal-screen-in-rust-after-a-new-line-is-printed
     println!("{} seconds to go.", time_remaining);
 }
-*/
+
+//use std::fs;
+// fn temp_read_file_and_print(filename: &str) {
+//     let contents = fs::read_to_string(filename).expect("Unable to read file");
+//     println!("Contents of file {}", filename);
+//     println!("{}", contents);
+// }
