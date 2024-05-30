87aaecab7d4dfdd4fad289dae73269ad343e311c
Liam Swain <liamswain4@gmail.com>
2024-05-30 22:36:32 +0100

    Implemented basic timer which can read from a toml config

--- Notes.md
+++ Notes.md
@@ -1,1 +1,57 @@
 # General Gist
+Print current time, print progress bar? How to work out the percentage?
+
+If current percentage:
+0% - 9%: no bars
+10% - 19%: 1 bar
+20% - 29%: 2 bars
+30% - 39%: 3 bars
+40% - 49%: 4 bars
+50% - 59%: 5 bars
+60% - 69%: 6 bars
+70% - 79%: 7 bars
+80% - 89%: 8 bars
+90% - 99%: 9 bars
+100%: 10 bars (finished)
+
+[==========] 100%
+100% [==========]
+
+5 mins remaining.
+85% [========  ]
+[========  ] 85%
+
+Here's the format we want:
+5 mins remaining.
+[========  ] 85%
+
+Do we want to save completed timer things to a file?
+What do we want to load from a config? (json, yaml, toml even?)
+We want these to be configurable from the commandline too!
+
+rustodoro --set-work-time --minutes 25
+rustodoro --set-work-time --seconds 1500
+rustodoro --set-short-break-time --minutes 5
+rustodoro --set-short-break-time --seconds 300
+rustodoro --set-long-break-time --minutes 15
+rustodoro --set-long-break-time --seconds 900
+
+VIM Note: This is early days, Liam! There's going to be some teething issues/friction initially but you're going to learn as you go. You got this! You can learn this!
+
+How do I do colours when writing to terminal?
+
+How do we figure out percentages again?
+(50/100)*100 = 50
+(val/total)*100 = percentage
+
+How to do the progress bar...
+10 spaces in total.
+Divide current number by 10?
+
+55/10 = 5
+5 =, 10 - G5 spaces. 
+
+WAIT, I have this the wrong way round.
+
+Add in commands to configure!
+When we finish a work stint, print out that we can have a break now + how many work stints we've taken?
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use std::time::Instant;
+use colored::Colorize;
+use serde::Serialize;
+use serde::Deserialize;
+use std::fs;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    work_time: u64,
+    short_break_time: u64,
+    long_break_time: u64,
+    pomodoros_till_long_break: u64,
+    display_secs: bool,
+}
+
+const CONFIG_PATH: &str = "./Config.toml";
+
 fn main() {
-    println!("Timer time!");
+    let contents = fs::read_to_string(CONFIG_PATH)
+        .expect(format!("Could not read file {}", CONFIG_PATH).as_str());
+    let config: Config = toml::from_str(&contents).unwrap();
+
+    // Parse commandline args here!
+    // Is the user starting a work timer, a short/long break timer, modifying their config?     
+
+    let start = Instant::now();
+    print_time_remaining(config.work_time, config.work_time);
+
+    // Work Timer
+    let mut old_printed_value: u64 = 0;
+    loop {
+        let elapsed_seconds = start.elapsed().as_secs();
+
+        if elapsed_seconds > old_printed_value {
+            let time_remaining = config.work_time - elapsed_seconds;
+            print_time_remaining(time_remaining, config.work_time);
+            old_printed_value = elapsed_seconds;
+        }
+
+        if elapsed_seconds >= config.work_time {
+            break;
+        }
+    }
+    
+    println!("Timer elapsed!");
+    // TODO: Save to file that we've done another work/break stint. Do we want to save logs per day? That might be best!
+    // Do we have a max size/amount of logs? Might be worth looking into later but don't worry for now.
+}
+
+fn print_time_remaining(time_remaining: u64, total_time: u64) {
+    print!("{esc}c", esc = 27 as char); // Hacky way of clearing the console! https://stackoverflow.com/questions/34837011/how-to-clear-the-terminal-screen-in-rust-after-a-new-line-is-printed 
+    println!("{} seconds to go.", time_remaining); // Do this in mins? Allow use to config!
+
+    let percentage: u64 = (100.0 - ((time_remaining as f64/total_time as f64) * 100.0)) as u64;
+    let mut progress_bar: String = String::new();
+    let progress_amount = percentage/10;
+    let space_amount = 10 - progress_amount;
+    
+    if progress_amount > 0 {
+        for _i in 0..progress_amount {
+            progress_bar += "=";
+        }
+    }
+
+    if space_amount > 0 {
+        for _i in 0..space_amount {
+            progress_bar += " ";
+        }
+    }
+
+    println!("[{}] {}%", progress_bar.green(), percentage);
 }
