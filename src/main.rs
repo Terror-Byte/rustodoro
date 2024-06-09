069cafcc5306881a6d24bdd368b400d0d862197a
Liam Swain <liamswain4@gmail.com>
2024-06-09 15:32:26 +0100

    Tidied code that'd been moved

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,11 @@
 mod args;
 mod rustodoro_config;
 
-// use std::cmp;
 use std::io::stdout;
 use std::io::Write;
 use std::time::Instant;
-// use args::SetWorkTimerCommand;
 use crossterm::queue;
 use crossterm::style;
-// use serde::Serialize;
-// use serde::Deserialize;
-// use std::fs;
 use crossterm::terminal::{ 
     Clear, 
     ClearType 
@@ -25,117 +20,7 @@ use crossterm::style::{
 use args::RustodoroArgs;
 use args::RustodoroCommand;
 use clap::Parser;
-
-//mod config;
 use rustodoro_config::RustodoroConfig;
-
-// // TODO: Do we want the config to be aware of its own path?
-// #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-// struct Config {
-//     work_time: u16,
-//     short_break_time: u16,
-//     long_break_time: u16,
-//     pomodoros_till_long_break: u16,
-//     display_in_secs: bool,
-// }
-
-// impl Config {
-//     // TODO: Should saving and loading be a method or a static function?
-//     // TODO: Have this return errors OR just panic?
-//     //fn save(&self, config_path: &str) {
-//     fn save(config: &Config, config_path: &str) {
-//         //let contents = toml::to_string(config);
-//         let contents = toml::to_string(config)
-//             .expect("Could not parse config as string.");
-//         fs::write(config_path, contents.as_str())
-//             .expect(format!("Could not write to file {}", config_path).as_str());
-//         // if let Ok(foo) = contents {
-//         //     // TODO: Probs not the best way to handle an error, but what else do we want to do?
-//         //     fs::write(config_path, foo.as_str())
-//         //         .expect(format!("Could not write to file {}", config_path).as_str());
-//         // }
-//     }
-
-//     // TODO: Have this handle errors gracefully or just have it panic if things aren't right?
-//     // To be fair, if we have errors reading or parsing we don't /want/ the program to continue.
-//     fn load(config_path: &str) -> Config {
-//         let contents = fs::read_to_string(config_path)
-//             .expect(format!("Could not read file {}", config_path).as_str());
-//         let config: Config = toml::from_str(&contents)
-//             .unwrap();
-//         config
-//     }
-
-//     // Make new config!
-//     fn set_work_timer(self, command: SetWorkTimerCommand) -> Config {
-//         // This is in seconds!
-//         let mut work_timer: u16 = 0;
-
-//         if let Some(minutes) = command.minutes {
-//             work_timer += minutes * 60;// TODO: Do we want the config to be aware of its own path?
-// #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
-// struct Config {
-//     work_time: u16,
-//     short_break_time: u16,
-//     long_break_time: u16,
-//     pomodoros_till_long_break: u16,
-//     display_in_secs: bool,
-// }
-
-// impl Config {
-//     // TODO: Should saving and loading be a method or a static function?
-//     // TODO: Have this return errors OR just panic?
-//     //fn save(&self, config_path: &str) {
-//     fn save(config: &Config, config_path: &str) {
-//         //let contents = toml::to_string(config);
-//         let contents = toml::to_string(config)
-//             .expect("Could not parse config as string.");
-//         fs::write(config_path, contents.as_str())
-//             .expect(format!("Could not write to file {}", config_path).as_str());
-//         // if let Ok(foo) = contents {
-//         //     // TODO: Probs not the best way to handle an error, but what else do we want to do?
-//         //     fs::write(config_path, foo.as_str())
-//         //         .expect(format!("Could not write to file {}", config_path).as_str());
-//         // }
-//     }
-
-//     // TODO: Have this handle errors gracefully or just have it panic if things aren't right?
-//     // To be fair, if we have errors reading or parsing we don't /want/ the program to continue.
-//     fn load(config_path: &str) -> Config {use args::SetWorkTimerCommand;
-//             .unwrap();
-//         config
-//     }
-
-//     // Make new config!
-//     fn set_work_timer(self, command: SetWorkTimerCommand) -> Config {
-//         // This is in seconds!
-//         let mut work_timer: u16 = 0;
-
-//         if let Some(minutes) = command.minutes {
-//             work_timer += minutes * 60;
-//         }
-
-//         if let Some(seconds) = command.seconds {
-//             match seconds {
-//                 0..=60 => work_timer += seconds as u16,
-//                 _ => println!("Error!") // TODO: What do we do in this case? Should this return a Result?
-//             }
-//         }
-
-//         Config { 
-//             work_time: work_timer,
-//             ..self
-//         }
-//     }
-// }
-//         }
-
-//         Config { 
-//             work_time: work_timer,
-//             ..self
-//         }
-//     }
-// }
 
 #[derive(Copy, Clone)]
 enum TimerType {
@@ -147,9 +32,6 @@ enum TimerType {
 const CONFIG_PATH: &str = "./Config.toml";
 
 fn main() -> Result<(), std::io::Error> {
-    // let contents = fs::read_to_string(CONFIG_PATH)
-    //     .expect(format!("Could not read file {}", CONFIG_PATH).as_str());
-    // let config: Config = toml::from_str(&contents).unwrap();
     let config = RustodoroConfig::load(CONFIG_PATH);
 
     // TODO: What errors do we want these functions all to throw? Do we want them all to be propagatable updwards (if that's even a phrase)?
@@ -161,7 +43,6 @@ fn main() -> Result<(), std::io::Error> {
         RustodoroCommand::SetWorkTimer(command) => {
             let new_config = config.set_work_timer(command);
             RustodoroConfig::save(&new_config, CONFIG_PATH);
-            //new_config.save(CONFIG_PATH); // We want this as its own function and not as part of set_work_timer, right?
         },
         RustodoroCommand::SetShortBreakTimer(command) => {
             println!("{:?}", command)
